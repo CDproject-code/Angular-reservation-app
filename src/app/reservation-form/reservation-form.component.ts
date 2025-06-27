03e54432f350001d40c4fb8c24fb5b3f74fff5f5
CDproject-code <127301540+CDproject-code@users.noreply.github.com>
2025-06-27 19:00:19 +0200

    Update reservation-form.component.ts

--- src/app/reservation-form/reservation-form.component.ts
+++ src/app/reservation-form/reservation-form.component.ts
@@ -22,9 +22,11 @@ export class ReservationFormComponent implements OnInit {
     let id = this.activatedRoute.snapshot.paramMap.get('id');
 
     if(id){
-      let reservation = this.reservationService.getReservation(id);
-      if(reservation) 
-        this.reservationForm.patchValue(reservation);
+      this.reservationService.getReservation(id).subscribe(reservation => {
+        if(reservation) {
+          this.reservationForm.patchValue(reservation);
+        }
+      });
     }
   }
 
@@ -48,10 +50,14 @@ export class ReservationFormComponent implements OnInit {
       if(id){
         // Update existing reservation
         reservation.id = id;
-        this.reservationService.updateReservation(id, reservation);
+        this.reservationService.updateReservation(id, reservation).subscribe(()=> {
+          console.log(`Reservation with id ${id} updated`);
+        });
       } else {
         // Add new reservation
-        this.reservationService.addReservation(reservation);
+        this.reservationService.addReservation(reservation).subscribe(() => {
+          console.log('New reservation added');
+        });
       }
 
       this.router.navigate(['/list']);
